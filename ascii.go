ac8882f9e0110c73d01a6386ea51ce2282661277
Leonid Kneller <recondite.matter@gmail.com>
2020-10-22 09:20:36 -0400

    Add copyright (c).

--- ascii.go
+++ ascii.go
@@ -1,3 +1,7 @@
+// Copyright (c) 2019-2020 Leonid Kneller. All rights reserved.
+// Licensed under the MIT license.
+// See the LICENSE file for full license information.
+
 // Package ascii -- provides ASCII-only character functions.
 package ascii
 
